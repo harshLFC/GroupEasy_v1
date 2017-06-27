d64494616fb650c4db84aaafe428592efe7896a9
harsh_elnino <d16123828@mydit.ie>
2017-06-27 20:14:21 +0100

    Login screen succesfully linked to chat , tabs with bad interface working

--- app/src/main/java/com/example/lenovo/groupeasy_v1/Tab1Main.java
+++ app/src/main/java/com/example/lenovo/groupeasy_v1/Tab1Main.java
@@ -1,11 +1,18 @@
 package com.example.lenovo.groupeasy_v1;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -15,7 +22,7 @@ import com.google.firebase.auth.FirebaseAuth;
  * Created by lenovo on 26-06-2017.
  */
 
-public class Tab1Main extends Fragment{
+public class Tab1Main extends Fragment {
 
     private Context mContext;
     private static final String TAG = "GoogleActivity";
@@ -35,6 +42,7 @@ public class Tab1Main extends Fragment{
         mContext = getActivity();
         View rootView = inflater.inflate(R.layout.tab1_main, container, false);
 
+
 //        // Views
 //        mStatusTextView = (TextView) rootView.findViewById(R.id.status);
 //        mDetailTextView = (TextView) rootView.findViewById(R.id.detail);
@@ -44,6 +52,25 @@ public class Tab1Main extends Fragment{
 //       rootView.findViewById(R.id.go_to_chat).setOnClickListener((View.OnClickListener) this);
 //       rootView.findViewById(R.id.disconnect_button).setOnClickListener((View.OnClickListener) this);
 
+
+//        //attempt to paint something on canvas
+//
+//        Paint paint = new Paint();
+//        paint.setColor(Color.parseColor("#D3D3D3"));
+//        Bitmap bg = Bitmap.createBitmap(480, 800, Bitmap.Config.ARGB_8888);
+//
+//            Canvas canvas = new Canvas(bg);
+//            RectF rectF = new RectF(50,20,100,80);
+//            canvas.drawArc(rectF,90,45,true,paint);
+////        LinearLayout ll = (LinearLayout) findViewById(R.id.draw_area);
+//        LinearLayout ll = (LinearLayout) rootView.findViewById(R.id.draw_area);
+////        return new BitmapDrawable(mContext.getResources(), bg);
+//        ll.setBackground(new BitmapDrawable(bg));
+//
+////        Failed attempt
+
         return rootView;
     }
 }
+
+
